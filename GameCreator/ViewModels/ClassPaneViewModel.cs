af78a428f148d80a5c5e37571028e8b8e239007b
ep9869 <eli.puschtra@gmail.com>
2014-10-08 16:39:01 +0200

    Now buildable

---
+++ GameCreator/ViewModels/ClassPaneViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCreator.ViewModels
+{
+    class ClassPaneViewModel : PaneViewModel
+    {
+        public ClassPaneViewModel(GC_Class gcclass)
+        {
+            if (gcclass == null)
+                throw new ArgumentNullException("gcclass");
+            this.Item = gcclass;
+            this.Title = gcclass.Name;
+        }
+
+        public override void AssignChanges()
+        {
+            (Item as GC_Class).Name = this.Title;
+        }
+    }
+}
---
+++ GameCreator/Views/Pane/PanesTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Wpf.AvalonDock.Layout;
+using System.Windows;
+using System.Windows.Controls;
+using GameCreator.ViewModels;
+
+namespace GameCreator.Views.Pane
+{
+    class PanesTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ClassPaneTemplate { get; set; }
+        public DataTemplate ImagePaneTemplate { get; set; }
+        public DataTemplate ObjectPaneTemplate { get; set; }
+        public DataTemplate LevelPaneTemplate { get; set; }
+        
+        public override DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+        {
+            if (item is ClassPaneViewModel)
+                return ClassPaneTemplate;
+            if (item is ImagePaneViewModel)
+                return ImagePaneTemplate;
+            if (item is ObjectPaneViewModel)
+                return ObjectPaneTemplate;
+            if (item is LevelPaneViewModel)
+                return LevelPaneTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
